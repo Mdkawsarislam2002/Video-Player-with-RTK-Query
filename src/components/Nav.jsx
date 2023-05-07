d561ec1c4710fbe9e3862d86b895b7d03aa12139
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-05-07 22:29:53 +0600

    converted video section , html into jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import lws from "../assets/lws.svg";
 import search from "../assets/search.svg";
 
@@ -6,9 +7,9 @@ const Nav = () => {
     <div>
       <nav className="bg-slate-100 shadow-md">
         <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
-          <a href="/">
+          <Link to="/">
             <img className="h-10" src={lws} alt="Learn with Sumit" />
-          </a>
+          </Link>
           <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
             {/* search */}
             <form>
--- src/pages/SingleVideo.jsx
+++ src/pages/SingleVideo.jsx
@@ -1,5 +1,59 @@
+import Nav from "../components/Nav";
+
+import RelatedVideos from "./RelatedVideos";
+import LinkAndUnlike from "./LinkAndUnlike";
+
+import { useGetSingleVideoQuery } from "../Redux/features/apiSlice/apiSlice";
+import { useParams } from "react-router-dom";
+
 const SingleVideo = () => {
-  return <div>SingleVideo</div>;
+  const { id } = useParams();
+
+  const { data } = useGetSingleVideoQuery(id);
+
+  return (
+    <>
+      <Nav />
+
+      <section className="pt-6 pb-20">
+        <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+          <div className="grid grid-cols-3 gap-2 lg:gap-8">
+            <div className="col-span-full w-full space-y-8 lg:col-span-2">
+              <iframe
+                width="100%"
+                className="aspect-video"
+                src={data?.link}
+                title="Some video title"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+
+              <div>
+                <h1 className="text-lg font-semibold tracking-tight text-slate-800">
+                  {data?.title}
+                </h1>
+                <div className="pb-4 flex items-center space-between border-b">
+                  <h2 className="text-sm leading-[1.7142857] text-slate-600 w-full">
+                    Uploaded on 23 Nov 2022
+                  </h2>
+
+                  {/* like/unlike */}
+                  <LinkAndUnlike />
+                </div>
+
+                <div className="mt-4 text-sm text-[#334155] dark:text-slate-400">
+                  {data?.description}
+                </div>
+              </div>
+            </div>
+
+            {/* related videos */}
+            <RelatedVideos />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SingleVideo;
